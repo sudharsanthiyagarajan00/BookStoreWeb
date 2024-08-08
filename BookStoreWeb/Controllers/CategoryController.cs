79582c858cc96e232953e2cbffdc2812f8ce848c
T <2128145@cognizant.com>
2024-08-08 19:45:58 +0530

    Two

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -18,5 +18,23 @@ namespace BookStoreWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+        }
     }
 }
--- BookStoreWeb/Models/Category.cs
+++ BookStoreWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStoreWeb.Models
 {
@@ -8,8 +9,12 @@ namespace BookStoreWeb.Models
         public int Id { get; set; }
 
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string? Name { get; set; }
 
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order Must between 1-100")]
         public int? DisplayOrder { get; set; }
     }
 }
