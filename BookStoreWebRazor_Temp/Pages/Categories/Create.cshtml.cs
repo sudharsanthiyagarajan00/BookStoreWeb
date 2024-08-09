0377e9ac371842128f4fb0952ee70450b0ea826d
T <2128145@cognizant.com>
2024-08-09 19:20:15 +0530

    Razor_Commit_Create_Edit_Delete

---
+++ BookStoreWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,34 @@
+using BookStoreWebRazor_Temp.Data;
+using BookStoreWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookStoreWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category Created Successfully";
+            return RedirectToPage("Index");
+           
+           
+        }
+    }
+}
---
+++ BookStoreWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,43 @@
+using BookStoreWebRazor_Temp.Data;
+using BookStoreWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;  
+
+namespace BookStoreWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if(id != null && id!=0)
+            {
+                Category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToPage("Index");
+
+            }
+            return Page();
+        }
+    }
+}
+
+
