c0e9445391e87118b6de2173aef28ca633440b0d
T <2128145@cognizant.com>
2024-08-16 14:58:14 +0530

    commitUptoIdentity_Models

---
+++ BookStoreWeb.Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStoreWeb.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+
+        [NotMapped]
+        public string Role { get; set; }
+
+
+    }
+
+}
