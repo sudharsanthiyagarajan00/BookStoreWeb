af1337d43df31ff9aaf01f470de33e4e30836636
T <2128145@cognizant.com>
2024-08-27 15:10:40 +0530

    commitForDeployment

---
+++ BookStoreWeb.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,79 @@
+﻿using BookStoreWeb.DataAccess.Data;
+using BookStoreWeb.Models;
+using BookStoreWeb.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreWeb.DataAccess.DbInitializer
+{
+    public class DbInitializer:IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext db)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _db = db;
+        }
+
+
+        public void Initialize()
+        {
+
+
+            //migrations if they are not applied
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex) { }
+
+
+
+            //create roles if they are not created
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+
+
+                //if roles are not created, then we will create admin user as well
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "Admin00@gmail.com",
+                    Email = "Admin00@gmail.com",
+                    Name = "Sudharsan",
+                    PhoneNumber = "1112223333",
+                    StreetAddress = "Main Road",
+                    State = "TN",
+                    PostalCode = "625517",
+                    City = "Theni"
+                }, "Admin@00").GetAwaiter().GetResult();
+
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@dotnetmastery.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+
+            }
+
+            return;
+        }
+    }
+}
+
---
+++ BookStoreWeb.DataAccess/DbInitializer/IDbInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreWeb.DataAccess.DbInitializer
+{
+    public interface IDbInitializer
+    {
+        void Initialize();
+
+    }
+}
